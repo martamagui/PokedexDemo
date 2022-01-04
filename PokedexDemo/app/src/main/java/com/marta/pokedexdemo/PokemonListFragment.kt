1c63a5d369ef57699f3aa15a788924df4ee38410
Marta Molina <marta.molina@live.u-tad.com>
2022-01-04 13:58:34 +0100

    SecurityConfig

--- PokedexDemo/app/src/main/java/com/marta/pokedexdemo/PokemonListFragment.kt
+++ PokedexDemo/app/src/main/java/com/marta/pokedexdemo/PokemonListFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.marta.pokedexdemo.databinding.FragmentPokemonListBinding
 
 
 class PokemonListFragment : Fragment() {
-
+    private var _binding: FragmentPokemonListBinding? = null
+    private val binding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +20,9 @@ class PokemonListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_pokemon_list, container, false)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 }
