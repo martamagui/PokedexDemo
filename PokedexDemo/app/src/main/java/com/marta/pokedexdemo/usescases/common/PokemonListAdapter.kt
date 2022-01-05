ebfe610954dcd3df37d3c1cc85e183a1167147b6
Marta Molina <marta.molina@live.u-tad.com>
2022-01-06 00:44:10 +0100

    Pokemons types are visible. Need to implement iv Glide function

--- PokedexDemo/app/src/main/java/com/marta/pokedexdemo/usescases/common/PokemonListAdapter.kt
+++ PokedexDemo/app/src/main/java/com/marta/pokedexdemo/usescases/common/PokemonListAdapter.kt
@@ -1,12 +1,18 @@
 package com.marta.pokedexdemo.usescases.common
 
+import android.opengl.Visibility
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet.GONE
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.marta.pokedexdemo.databinding.ItemPokemonBinding
 import com.marta.pokedexdemo.model.pokemon.Pokemon
+import java.util.*
 
 
 class PokemonListAdapter() :
@@ -20,8 +26,17 @@ class PokemonListAdapter() :
 
     override fun onBindViewHolder(holder: PokemonListAdapter.ViewHolder, position: Int) {
         val pokemon = getItem(position)
-        holder.binding.tvPokemonId.text = pokemon.id.toString()
-        holder.binding.tvNameCard.text = pokemon.name
+
+        holder.binding.tvPokemonId.text = "# ${pokemon.id.toString()}"
+        holder.binding.tvNameCard.text =
+            pokemon.name.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+        holder.binding.tvType1.text =  pokemon.types[0].type.name
+        if (pokemon.types.size>1){
+            holder.binding.tvType2.text = pokemon.types[1].type.name
+        }else{
+            holder.binding.tvType2.visibility = View.GONE
+        }
+
 
     }
 
